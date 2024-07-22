fb7d144675056080fe8cf65f3942c0831ab95bcd
Starikova Maria <maria050200@gmail.com>
2024-07-22 17:25:36 +0500

    Сделала компонент Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from '../Header/Header.jsx';
 import Main from '../Main/Main.jsx';
+import Footer from '../Footer/Footer.jsx';
 
 function App() {
   return (
     <div className="page">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 import './Footer.scss';
 
 function Footer() {
+  const handlePhoneClick = () => {
+    window.location.href = 'tel:+79320108888';
+  };
+
+//Можно сделать отправку сообщения на WhatsApp
+//   const handleWhatsAppClick = () => {
+//     const phoneNumber = '89823585320'; 
+//     const message = 'Привет, я хочу записаться на консультацию'; 
+//     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+//     window.open(url, '_blank');
+//   };
+
   return (
-    <section className="footer">
-      
-    </section>
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__info">
+          <p className="footer__title">Наш адрес</p>
+          <p className="footer__adress">г. Оренбург, ул. Лесозащитная, д. 20</p>
+        </div>
+        <button className="footer__button-phone" onClick={handlePhoneClick}>
+          <p className="footer__subtitle_btn">Записаться</p>
+        </button>
+      </div>
+      <p className="footer__text">&#169; 2017-2024. ООО "ОренФургон"</p>
+    </footer>
   );
 }
 
